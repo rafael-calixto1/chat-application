e2d6b8394699fd4eb006f1420bc145910d9ec546
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-07-21 17:44:05 -0300

    project setup

---
+++ backend/src/main/java/com/dsenvolvendosistemas/chat_application/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.dsenvolvendosistemas.chat_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}

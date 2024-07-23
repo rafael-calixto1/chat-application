295a1aaab13ce971d6dd9975d178d41f32867765
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-07-22 21:26:55 -0300

    create-user-service

---
+++ backend/src/main/java/com/dsenvolvendosistemas/chat_application/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dsenvolvendosistemas.chat_application.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findAllByStatus(Status status);
+}
